9f66c482401aaef6e5204ffe15ece214f89b2067
rossjbm <118684575+rossjbm@users.noreply.github.com>
2024-03-16 22:51:34 -0400

    Estilos Encabezado

--- FRONTEND/src/componentes/Encabezado/Encabezado.jsx
+++ FRONTEND/src/componentes/Encabezado/Encabezado.jsx
@@ -18,52 +18,53 @@ export function Encabezado() {
         <ModoOscuro>
             {(isDarkMode) => (
                 <header >
-                    <div className="px-10 py-4 relative h-20">
+                    <div className={`px-10 py-4 relative h-20 ${isDarkMode ? 'bg-dark-blueOscuro' : ''}`}>
                         <div className="flex flex-row justify-between items-center">
-                            <Link to={`/inicio`}className="no-underline text-black"><h1 className="text-2xl font-textos no-underline text-black">Hazbin<span className="no-underline font-titulos text-[28px]">Hotel</span></h1></Link>
+                            <Link to={`/inicio`} className={`no-underline ${isDarkMode ? 'text-white' : 'text-black'}`}>
+                                <h1 className={`text-2xl font-textos no-underline ${isDarkMode ? 'text-white' : 'text-black'}`}>Hazbin<span className="no-underline font-titulos text-[28px]">Hotel</span></h1></Link>
                             <div className="flex flex-row justify-end items-center gap-3">
                                 <nav className="hidden md:flex flex-row gap-16 items-center justify-center ">
                                     <ul className="flex flex-row gap-16 items-center justify-end md:justify-center font-textos text-lg">
                                         <li className="hidden lg:inline-block">
-                                            <NavLink to={`/inicio`} className={({isActive}) => (isActive ? "font-bold border-b-2 border-blue-200 p-2 no-underline text-black" : "no-underline text-black")} >Inicio</NavLink>
+                                            <NavLink to={`/inicio`} className={({isActive}) => (isActive ? `font-bold border-b-2 p-2 no-underline  ${isDarkMode ? 'text-white border-dark-greenClaro' : 'text-black border-blue-200'}` : `no-underline ${isDarkMode ? 'text-white' : 'text-black'}`)} >Inicio</NavLink>
                                         </li>
                                         <li className="hidden lg:inline-block">
-                                            <NavLink to={`/cerrar`} className={({isActive}) => (isActive ? "font-bold border-b-2 border-blue-200 p-2 no-underline text-black" : "no-underline text-black")}>Cerrar Sesión</NavLink>
+                                            <NavLink to={`/cerrar`} className={({isActive}) => (isActive ? `font-bold border-b-2 p-2 no-underline  ${isDarkMode ? 'text-white border-dark-greenClaro' : 'text-black border-blue-200'}` : `no-underline ${isDarkMode ? 'text-white' : 'text-black'}`)} >Cerrar Sesión</NavLink>
                                         </li>
                                         <li className="hidden md:inline-block">
-                                            <NavLink to={`/reservar`} className={({isActive}) => (isActive ? "font-bold bg-blue-200 py-[11px] px-5 no-underline text-black" : "bg-black text-white py-[11px] px-5 hover:bg-blue-200 no-underline hover:text-black")}>Reservar</NavLink>
+                                            <NavLink to={`/reservar`} className={({isActive}) => (isActive ? `font-bold  py-[11px] px-5 no-underline text-black ${isDarkMode ? 'text-white bg-dark-greenClaro' : 'text-black bg-blue-200'}` : `py-[11px] px-5  no-underline ${isDarkMode ? 'text-black bg-white hover:bg-dark-greenClaro' : 'text-white bg-black hover:bg-blue-200 hover:text-black'}`)}>Reservar</NavLink>
                                         </li>
                                     </ul>
                                 </nav>
         
                                 {menu ?
                                     <>
-                                    <motion.button onClick={() => {setMenu(false)}}  initial={{ opacity: 0, scale:0.9 }} animate={{ opacity: 1, scale:1}} transition={{ duration: 0.3}} className="flex lg:hidden "><IoCloseOutline className="text-5xl"/></motion.button>
+                                    <motion.button onClick={() => {setMenu(false)}}  initial={{ opacity: 0, scale:0.9 }} animate={{ opacity: 1, scale:1}} transition={{ duration: 0.3}} className={`flex lg:hidden items-center ${isDarkMode ? 'text-white' : 'text-black'}`}><IoCloseOutline className="text-5xl"/></motion.button>
                                     </>
                                     :   
-                                    <motion.button onClick={() => {setMenu(true)}} initial={{ opacity: 0, scale:0.9 }} animate={{ opacity: 1, scale:1}} transition={{ duration: 0.3}} className="flex lg:hidden items-center" ><IoMenuOutline className="text-5xl"/></motion.button>
+                                    <motion.button onClick={() => {setMenu(true)}} initial={{ opacity: 0, scale:0.9 }} animate={{ opacity: 1, scale:1}} transition={{ duration: 0.3}} className={`flex lg:hidden items-center ${isDarkMode ? 'text-white' : 'text-black'}`}  ><IoMenuOutline className="text-5xl"/></motion.button>
                                 }
                             </div>
                         </div>
                         <Menu menu={menu} setMenu={setMenu} />
                     </div>
-                    <div className="border-y border-black">
-                        <nav className="py-3 px-5 md:px-10">
+                    <div className={`border-y ${isDarkMode ? 'bg-dark-blueOscuro border-white' : 'border-black'}`}>
+                        <nav className="py-3 px-5 md:px-10  md:overflow-x-hidden">
                             <ul style={{}}  className="flex w-full md:justify-around overflow-x-scroll md:overflow-x-hidden overflow-y-hidden gap-20 text-lg font-textos h-10 md:h-auto">
                                 <li className="flex-none w-30 scroll-snap-align-center no-underline text-black">
-                                    <NavLink to={`/contacto`} className={({isActive}) => (isActive ? "font-bold border-b-2 border-blue-200 no-underline text-black" : "no-underline text-black")} >Contacto</NavLink>
+                                    <NavLink to={`/contacto`} className={({isActive}) => (isActive ? `font-bold border-b-2  no-underline ${isDarkMode ? 'text-white border-dark-greenClaro' : 'border-blue-200 text-black'}` : `no-underline ${isDarkMode ? 'text-white' : 'text-black'}`)} >Contacto</NavLink>
                                 </li>
                                 <li className="flex-none w-30 scroll-snap-align-center">
-                                    <NavLink to={`/ofertas`} className={({isActive}) => (isActive ? "font-bold border-b-2 border-blue-200 no-underline text-black" : "no-underline text-black")} >Ofertas</NavLink>
+                                    <NavLink to={`/ofertas`} className={({isActive}) => (isActive ? `font-bold border-b-2  no-underline ${isDarkMode ? 'text-white border-dark-greenClaro' : 'border-blue-200 text-black'}` : `no-underline ${isDarkMode ? 'text-white' : 'text-black'}`)} >Ofertas</NavLink>
                                 </li>
                                 <li className="flex-none w-30 scroll-snap-align-center">
-                                    <NavLink to={`/habitaciones`} className={({isActive}) => (isActive ? "font-bold border-b-2 border-blue-200 no-underline text-black" : "no-underline text-black")}>Habitaciones</NavLink>
+                                    <NavLink to={`/habitaciones`} className={({isActive}) => (isActive ? `font-bold border-b-2  no-underline ${isDarkMode ? 'text-white border-dark-greenClaro' : 'border-blue-200 text-black'}` : `no-underline ${isDarkMode ? 'text-white' : 'text-black'}`)}>Habitaciones</NavLink>
                                 </li>
                                 <li className="flex-none w-30 scroll-snap-align-center">
-                                    <NavLink to={`/blog`} className={({isActive}) => (isActive ? "font-bold border-b-2 border-blue-200 no-underline text-black" : "no-underline text-black")}>Blog</NavLink>
+                                    <NavLink to={`/blog`} className={({isActive}) => (isActive ? `font-bold border-b-2  no-underline ${isDarkMode ? 'text-white border-dark-greenClaro' : 'border-blue-200 text-black'}` : `no-underline ${isDarkMode ? 'text-white' : 'text-black'}`)}>Blog</NavLink>
                                 </li>
                                 <li className="flex-none w-30 scroll-snap-align-center">
-                                    <NavLink to={`/pagos`} className={({isActive}) => (isActive ? "font-bold border-b-2 border-blue-200 no-underline text-black" : "no-underline text-black")}>Pagos</NavLink>
+                                    <NavLink to={`/pagos`} className={({isActive}) => (isActive ? `font-bold border-b-2  no-underline ${isDarkMode ? 'text-white border-dark-greenClaro' : 'border-blue-200 text-black'}` : `no-underline ${isDarkMode ? 'text-white' : 'text-black'}`)}>Pagos</NavLink>
                                 </li>
                             </ul>
                         </nav>
--- FRONTEND/src/componentes/Encabezado/Menu.jsx
+++ FRONTEND/src/componentes/Encabezado/Menu.jsx
@@ -1,31 +1,35 @@
 import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
-
+import ModoOscuro from "../Modo Oscuro/ModoOscuro";
 
 export function Menu({menu, setMenu}) {
     return(<>
-        <AnimatePresence>
-        {
-            menu ?
-                <>
-                    <motion.div className="bg-white lg:hidden flex flex-col items-center absolute top-20 right-0 z-10 w-full border-t border-b border-gray-200" initial={{ opacity: 0, y: -5 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} exit={{opacity: 0, y: -10 }}>
-                        <nav className="py-6 px-10 w-full">
-                            <ul className="flex flex-col gap-3 align-center">
-                                <li className="text-lg font-textos">
-                                    <Link to={`/inicio`} className="no-underline text-black">Inicio</Link>
-                                </li>
-                                <li className="text-lg font-textos">
-                                    <Link to={`/acceder`} className="no-underline text-black">Cerrar Sesión</Link>
-                                </li>
-                            </ul>
-                        </nav>
-                        <div className="border-t border-gray-200 w-4/5 p-4">
-                            <h3 className="text-center font-textos text-sm font-semibold">+58 412 4657234 - Hazbin<span className="font-titulos text-base">Hotel</span></h3>
-                        </div>
-                    </motion.div>
-                </> 
-            : null
-        }
-        </AnimatePresence>
+        <ModoOscuro>
+            {(isDarkMode) => (
+                <AnimatePresence>
+                {
+                    menu ?
+                        <>
+                            <motion.div className={`lg:hidden flex flex-col items-center absolute top-20 right-0 z-10 w-full border-t border-b ${isDarkMode ? 'bg-dark-blueOscuro border-white text-white' : 'bg-white  border-gray-200 text-black'}`} initial={{ opacity: 0, y: -5 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} exit={{opacity: 0, y: -10 }}>
+                                <nav className="py-6 px-10 w-full">
+                                    <ul className="flex flex-col gap-3 align-center">
+                                        <li className="text-lg font-textos">
+                                            <Link to={`/inicio`} className="no-underline ">Inicio</Link>
+                                        </li>
+                                        <li className="text-lg font-textos">
+                                            <Link to={`/acceder`} className="no-underline ">Cerrar Sesión</Link>
+                                        </li>
+                                    </ul>
+                                </nav>
+                                <div className="border-t border-gray-200 w-4/5 p-4">
+                                    <h3 className="text-center font-textos text-sm">+58 412 4657234 - Hazbin<span className="font-titulos text-base">Hotel</span></h3>
+                                </div>
+                            </motion.div>
+                        </> 
+                    : null
+                }
+                </AnimatePresence>
+            )}
+        </ModoOscuro>
     </>)
 }
