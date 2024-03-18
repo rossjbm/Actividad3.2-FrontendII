d1bdd5cd664fe7b8dc34a3d8bda2eb1f4cdae836
JMMP33 <94369965+JMMP33@users.noreply.github.com>
2024-03-18 15:26:49 -0400

    Modificando ofertas

--- FRONTEND/src/paginas/Ofertas/Ofertas.jsx
+++ FRONTEND/src/paginas/Ofertas/Ofertas.jsx
@@ -1,71 +1,69 @@
 import React, { useState, useContext, useEffect } from 'react';
 import ofertasData from "../../data/Ofertas.json";
 
-//componentes
+// Componentes
 import { ListarOfertas } from '../../funciones/Fetch/Ofertas/ListarOfertas';
 import { RenderOfertas } from '../../componentes/Ofertas/RenderOfertas';
 import { AgregarOfertas } from '../../componentes/Ofertas/CRUD/AgregarOfertas';
 import Loader from '../../componentes/Globales/Loader/Loader';
 
-//modo oscuro
+// Modo oscuro
 import ModoOscuro from '../../componentes/Globales/Modo Oscuro/ModoOscuro';
 
-//contexto de la sesion activa
+// Contexto de la sesión activa
 import { Sesion } from '../../App';
 
 export function Ofertas() {
-  const {sesionActiva} = useContext(Sesion);
+  const { sesionActiva } = useContext(Sesion);
   const [ofertas, setOfertas] = useState();
   const [cargando, setCargando] = useState(false);
+  const [botonMostrar, setBotonMostrar] = useState(false); // Estado para mostrar el botón
 
-  useEffect( () => {
+  useEffect(() => {
     async function fetchData() {
-      setCargando(true)
+      setCargando(true);
       try {
-        console.log('estamos listando ofertas')
-        const documentos = await ListarOfertas()
+        console.log('Estamos listando ofertas');
+        const documentos = await ListarOfertas();
         await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log('soy doc', documentos.habitaciones)
-        setOfertas(documentos.habitaciones)
-      } catch(error) {
-        console.log(error)
+        console.log('Soy doc', documentos.habitaciones);
+        setOfertas(documentos.habitaciones);
+        setBotonMostrar(true); // Mostrar el botón después de cargar las ofertas
+      } catch (error) {
+        console.log(error);
       }
     }
     fetchData();
   }, []);
 
   useEffect(() => {
-    ofertas === undefined ? setCargando(true) : setCargando(false)
-    console.log('oferta es', Array.isArray(ofertas))
+    ofertas === undefined ? setCargando(true) : setCargando(false);
+    console.log('Oferta es', Array.isArray(ofertas));
   }, [ofertas]);
-
-
 
   return (
     <>
       <ModoOscuro>
         {(isDarkMode) => (
           <>
-            {/* titulo de página */}
+            {/* Titulo de página */}
             <div className="static h-56 bg-bgFondo1 bg-cover bg-fixed w-full">
               <div className="flex justify-center items-center w-full h-56 bg-gray-400 p-5">
                 <h2 className="text-white text-3xl font-textos text-center">Ofertas</h2>
               </div>
             </div>
 
-            {/* agregar ofertas */}
-            {sesionActiva === 2 ?
-              <AgregarOfertas/>
-              : null
-            }
+            {/* Agregar ofertas */}
+            {sesionActiva === 2 && botonMostrar && (
+              <AgregarOfertas />
+            )}
 
             {/* Renderizado Ofertas */}
-            {cargando || !ofertas ? 
-              <Loader/>
-              : 
-              // null
-              <RenderOfertas ofertas={ofertas} /> 
-            }
+            {cargando || !ofertas ? (
+              <Loader />
+            ) : (
+              <RenderOfertas ofertas={ofertas} />
+            )}
           </>
         )}
       </ModoOscuro>
